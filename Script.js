97f0d8453e8e71b8ed78acc38b8ec33b8a85ea64
Brielle Woods <brielle@Brielles-MacBook-Air.local>
2017-04-20 21:00:44 -0400

    drum kit corrections

--- Script.js
+++ Script.js
@@ -1,47 +1,23 @@
 var boomElement = document.getElementById('Boom')
 var BoomSound = document.getElementById('BoomSound')
 var playBoom = function () {
- console.log('I was clicked!');
-    BoomSound.play();
+  BoomSound.play();
 }
 boomElement.addEventListener('mouseenter', playBoom)
-
-var boomElement = document.getElementById('Boom')
-var BoomSound = document.getElementById('BoomSound')
-var playBoom = function () {
- console.log('I was clicked!');
-    BoomSound.play();
-}
 boomElement.addEventListener('click', playBoom)
 
 var clapElement = document.getElementById('Clap')
 var ClapSound = document.getElementById('ClapSound')
 var playClap = function () {
- console.log('I was clicked!');
-    ClapSound.play();
+  ClapSound.play();
 }
 clapElement.addEventListener('click', playClap)
-
-var clapElement = document.getElementById('Clap')
-var ClapSound = document.getElementById('ClapSound')
-var playClap = function () {
- console.log('I was clicked!');
-    ClapSound.play();
-}
 clapElement.addEventListener('mouseenter', playClap)
 
 var hihatElement = document.getElementById('Hihat')
 var HihatSound = document.getElementById('HihatSound')
 var playHihat = function () {
- console.log('I was clicked!');
-    hihatSound.play();
+  HihatSound.play();
 }
 hihatElement.addEventListener('click', playHihat)
-
-var hihatElement = document.getElementById('Hihat')
-var HihatSound = document.getElementById('HihatSound')
-var playHihat = function () {
- console.log('I was clicked!');
-    HihatSound.play();
-}
 hihatElement.addEventListener('mouseenter', playHihat)
